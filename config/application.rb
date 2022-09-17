67f11e03a1c860a39d9af11edcbd075bd13b5c94
ana sanots <anasoliveiraaa@gmail.com>
2022-09-17 10:49:25 -0600

    setting up sidekiq

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module RailsBackgroundJobs
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
+    config.active_job.queue_adapter = :sidekiq
+    # Concdfiguration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
